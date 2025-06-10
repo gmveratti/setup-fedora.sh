9927e046a518ea4fba9cc8e23405d06c663ec7b7
gmveratti <134892354+gmveratti@users.noreply.github.com>
2025-06-10 20:44:24 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -84,7 +84,7 @@ sudo dnf remove docker \
 sudo dnf -y install dnf-plugins-core
 sudo dnf-3 config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
 
-sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
 sudo systemctl enable --now docker
 
